67eb95116aed1606e7189027f8bec6d77249e277
kubavation <37585627+kubavation@users.noreply.github.com>
2023-05-18 20:26:18 +0200

    styling + sidenav

--- configuration-service-web/src/app/shared/shared.module.ts
+++ configuration-service-web/src/app/shared/shared.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import {MatToolbarModule} from "@angular/material/toolbar";
 import {MatIconModule} from "@angular/material/icon";
 import {MatButtonModule} from "@angular/material/button";
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 
 
@@ -15,7 +17,9 @@ import {MatButtonModule} from "@angular/material/button";
   exports: [
     MatToolbarModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    MatSidenavModule,
+    BrowserAnimationsModule
   ]
 })
 export class SharedModule { }
