016fece5a45dff6b212cb793f9ca4fd06bfb89cf
kubavation <37585627+kubavation@users.noreply.github.com>
2023-05-20 15:14:16 +0200

    context-storage service implementation

--- configuration-service-web/src/app/context/storage/context-storage.service.ts
+++ configuration-service-web/src/app/context/storage/context-storage.service.ts
@@ -1,7 +1,20 @@
 import { Injectable } from '@angular/core';
+import {Context} from "../model/context";
 
 @Injectable()
 export class ContextStorageService {
 
+  private readonly KEY = 'context';
+
   constructor() { }
+
+  public store(context: Context): void {
+    sessionStorage.setItem(this.KEY, context.name.toString());
+  }
+
+  public storedValue(): Context | undefined {
+    const contextName = sessionStorage.getItem(this.KEY);
+    return contextName ? {name: contextName} : undefined;
+  }
+
 }
