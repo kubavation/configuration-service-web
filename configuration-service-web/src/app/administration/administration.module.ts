f46ac00e47fb0e77b8fea0ca5841fd756c6b70d6
kubavation <37585627+kubavation@users.noreply.github.com>
2023-05-20 16:53:44 +0200

    administration routing

--- configuration-service-web/src/app/administration/administration.module.ts
+++ configuration-service-web/src/app/administration/administration.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import {SharedModule} from "../shared/shared.module";
 import {AdministrationRoutingModule} from "./administration-routing.module";
 import {ModulesComponent} from "./modules/modules.component";
+import {ModuleService} from "./modules/service/module.service";
 
 
 
@@ -14,6 +15,7 @@ import {ModulesComponent} from "./modules/modules.component";
     CommonModule,
     SharedModule,
     AdministrationRoutingModule
-  ]
+  ],
+  providers: [ModuleService]
 })
 export class AdministrationModule { }
