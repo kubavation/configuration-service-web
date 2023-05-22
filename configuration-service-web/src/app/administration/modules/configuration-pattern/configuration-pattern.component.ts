b017dc6438b3d99a371b4d089444e4b7b83a4f94
kubavation <37585627+kubavation@users.noreply.github.com>
2023-05-22 19:58:50 +0200

    configuration pattern - component implementation

--- configuration-service-web/src/app/administration/modules/configuration-pattern/configuration-pattern.component.ts
+++ configuration-service-web/src/app/administration/modules/configuration-pattern/configuration-pattern.component.ts
@@ -1,5 +1,12 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
 import {ModuleService} from "../service/module.service";
+import {ActivatedRoute} from "@angular/router";
+import {filter, map, switchMap} from "rxjs";
+import {MatPaginator} from "@angular/material/paginator";
+import {MatSort} from "@angular/material/sort";
+import {MatTableDataSource} from "@angular/material/table";
+import {Module} from "../model/module";
+import {ConfigPattern} from "../model/config-pattern";
 
 @Component({
   selector: 'app-configuration-pattern',
@@ -8,9 +15,31 @@ import {ModuleService} from "../service/module.service";
 })
 export class ConfigurationPatternComponent {
 
-  constructor(private moduleService: ModuleService) {}
+  dataSource$ = this.route.params
+    .pipe(
+      filter(params => !!params['module']),
+      switchMap(params => this.moduleService.configurationPatterns(params['module'])),
+      map(patterns => this.toDataSource(patterns))
+    )
 
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
 
+  dataSource: MatTableDataSource<ConfigPattern>;
+
+  readonly displayedColumns = ['position', 'name', 'description', 'defaultValue'];
+
+  constructor(private moduleService: ModuleService,
+              private route: ActivatedRoute) {
+
+  }
+
+  private toDataSource(configurationPatterns: ConfigPattern[]): MatTableDataSource<ConfigPattern> {
+    this.dataSource = new MatTableDataSource<ConfigPattern>(configurationPatterns);
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+    return this.dataSource;
+  }
 
 
 }
