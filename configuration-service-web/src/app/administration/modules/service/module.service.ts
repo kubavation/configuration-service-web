fd7aaecc2862482e9687745d967f41fb038a0e75
kubavation <37585627+kubavation@users.noreply.github.com>
2023-05-22 19:27:10 +0200

    change in moduleService - module endpoint

--- configuration-service-web/src/app/administration/modules/service/module.service.ts
+++ configuration-service-web/src/app/administration/modules/service/module.service.ts
@@ -1,12 +1,13 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {Module} from "../model/module";
+import {environment} from "../../../../environments/environment";
 
 @Injectable()
 export class ModuleService {
 
-  public modules$: Observable<Module[]> = this.http.get<Module[]>('/todo');
+  public modules$: Observable<Module[]> = this.http.get<Module[]>(`${environment.url}/modules`);
 
   constructor(private http: HttpClient) { }
 }
