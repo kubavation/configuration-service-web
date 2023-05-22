16e85083585accd5a33e2dbd9088ae4736f00f32
kubavation <37585627+kubavation@users.noreply.github.com>
2023-05-22 19:34:05 +0200

    init of configuration pattern component

---
+++ configuration-service-web/src/app/administration/modules/configuration-pattern/configuration-pattern.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-configuration-pattern',
+  templateUrl: './configuration-pattern.component.html',
+  styleUrls: ['./configuration-pattern.component.scss']
+})
+export class ConfigurationPatternComponent {
+
+}
