c96901427858a0f6480cf5aa62f37e40f8a48392
kubavation <37585627+kubavation@users.noreply.github.com>
2023-05-22 19:38:39 +0200

    configPattern model + service method

--- configuration-service-web/src/app/administration/modules/service/module.service.ts
+++ configuration-service-web/src/app/administration/modules/service/module.service.ts
@@ -3,6 +3,7 @@ import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {Module} from "../model/module";
 import {environment} from "../../../../environments/environment";
+import {ConfigPattern} from "../model/config-pattern";
 
 @Injectable()
 export class ModuleService {
@@ -10,4 +11,9 @@ export class ModuleService {
   public modules$: Observable<Module[]> = this.http.get<Module[]>(`${environment.url}/modules`);
 
   constructor(private http: HttpClient) { }
+
+  public configPattern(module: string): Observable<ConfigPattern[]> {
+    return this.http.get<ConfigPattern[]>(`${environment.url}/modules/${module}/configuration-pattern`);
+  }
+
 }
