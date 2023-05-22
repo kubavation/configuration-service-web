fdfecb68f972678a4283a75563262aaf3af05b19
kubavation <37585627+kubavation@users.noreply.github.com>
2023-05-22 19:41:27 +0200

    routing for config-pattern

--- configuration-service-web/src/app/administration/administration-routing.module.ts
+++ configuration-service-web/src/app/administration/administration-routing.module.ts
@@ -1,10 +1,12 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from "@angular/router";
 import {ModulesComponent} from "./modules/modules.component";
+import {ConfigurationPatternComponent} from "./modules/configuration-pattern/configuration-pattern.component";
 
 const routes: Routes = [
   {path: '', component: ModulesComponent},
-  {path: 'modules', component: ModulesComponent}
+  {path: 'modules', component: ModulesComponent},
+  {path: 'modules/:module/configuration-pattern', component: ConfigurationPatternComponent}
 ];
 
 @NgModule({
