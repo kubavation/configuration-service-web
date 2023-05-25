1869e0c1a0a4655fdcbde8e88e05dc9e7be16684
kubavation <37585627+kubavation@users.noreply.github.com>
2023-05-25 19:01:19 +0200

    init of context-modules module + component

---
+++ configuration-service-web/src/app/context-modules/context-modules/context-modules.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-context-modules',
+  templateUrl: './context-modules.component.html',
+  styleUrls: ['./context-modules.component.scss']
+})
+export class ContextModulesComponent {
+
+}
