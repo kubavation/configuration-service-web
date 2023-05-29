8eb1a3d0c2c07ef424b4f32bd6d9755c3a48a83c
kubavation <37585627+kubavation@users.noreply.github.com>
2023-05-29 20:16:27 +0200

    usage of contextModules service

--- configuration-service-web/src/app/context-modules/context-modules.component.ts
+++ configuration-service-web/src/app/context-modules/context-modules.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import {ContextBsService} from "../shared/context/service/context-bs.service";
+import {switchMap} from "rxjs";
+import {ContextModulesService} from "./service/context-modules.service";
 
 @Component({
   selector: 'app-context-modules',
@@ -8,8 +10,14 @@ import {ContextBsService} from "../shared/context/service/context-bs.service";
 })
 export class ContextModulesComponent {
 
+  contextModules$ = this.contextBsService.context$
+    .pipe(
+      switchMap(context => this.contextModulesService.contextModules(context.name))
+    )
 
-  constructor(private contextBsService: ContextBsService) {
+
+  constructor(private contextBsService: ContextBsService,
+              private contextModulesService: ContextModulesService) {
   }
 
 }
