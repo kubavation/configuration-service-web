3f25feadd434e14a1fd0b1da47f7f59cae6cb210
kubavation <37585627+kubavation@users.noreply.github.com>
2023-05-29 20:54:24 +0200

    basic logic with AvailableModule as datasource

--- configuration-service-web/src/app/context-modules/context-modules-modal/context-modules-modal.component.ts
+++ configuration-service-web/src/app/context-modules/context-modules-modal/context-modules-modal.component.ts
@@ -4,6 +4,10 @@ import {FormBuilder, Validators} from "@angular/forms";
 import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
 import {ConfigPattern} from "../../administration/modules/model/config-pattern";
 import {FormMode} from "../../shared/forms/form-mode";
+import {ModuleService} from "../../administration/modules/service/module.service";
+import {BehaviorSubject, combineLatest, map, Observable} from "rxjs";
+import {ContextModule} from "../model/context-module";
+import {AvailableModule} from "./model/available-module";
 
 @Component({
   selector: 'app-context-modules-modal',
@@ -14,11 +18,26 @@ export class ContextModulesModalComponent extends DialogComponent<ContextModules
 
   form = this.fb.group({})
 
+  private contextModulesSubject = new BehaviorSubject<ContextModule[]>([]);
+
+  dataSource$: Observable<AvailableModule[]> = combineLatest([this.modulesService.modules$, this.contextModulesSubject])
+    .pipe(
+      map(([modules, contextModules]) => {
+        return modules.map(module => {
+          return {
+            name: module.name,
+            active: contextModules.find(contextModule => contextModule.name == module.name) != null
+          }
+        });
+      })
+    )
+
   constructor(public override dialogRef: MatDialogRef<ContextModulesModalComponent>,
-              @Inject(MAT_DIALOG_DATA) public data: any,
+              private modulesService: ModuleService,
+              @Inject(MAT_DIALOG_DATA) public data: ContextModule[],
               private fb: FormBuilder) {
     super(dialogRef);
-
+    this.contextModulesSubject.next(data);
   }
 
   save(): void {
