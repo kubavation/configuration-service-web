2492a2f4eda241149cf7bf51233e80669aaf6cd1
kubavation <37585627+kubavation@users.noreply.github.com>
2023-05-29 20:21:03 +0200

    structure of context-modules component

--- configuration-service-web/src/app/context-modules/context-modules.component.ts
+++ configuration-service-web/src/app/context-modules/context-modules.component.ts
@@ -1,7 +1,11 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
 import {ContextBsService} from "../shared/context/service/context-bs.service";
-import {switchMap} from "rxjs";
+import {map, switchMap} from "rxjs";
 import {ContextModulesService} from "./service/context-modules.service";
+import {MatTableDataSource} from "@angular/material/table";
+import {ContextModule} from "./model/context-module";
+import {MatPaginator} from "@angular/material/paginator";
+import {MatSort} from "@angular/material/sort";
 
 @Component({
   selector: 'app-context-modules',
@@ -10,14 +14,36 @@ import {ContextModulesService} from "./service/context-modules.service";
 })
 export class ContextModulesComponent {
 
-  contextModules$ = this.contextBsService.context$
+  dataSource$ = this.contextBsService.context$
     .pipe(
-      switchMap(context => this.contextModulesService.contextModules(context.name))
+      switchMap(context => this.contextModulesService.contextModules(context.name)),
+      map(modules => this.toDataSource(modules))
     )
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+
+  dataSource: MatTableDataSource<ContextModule>;
+
+  readonly displayedColumns = ['position', 'name', 'description'];
+
+  selected: ContextModule | undefined;
 
 
   constructor(private contextBsService: ContextBsService,
               private contextModulesService: ContextModulesService) {
   }
+
+  private toDataSource(modules: ContextModule[]): MatTableDataSource<ContextModule> {
+    this.dataSource = new MatTableDataSource<ContextModule>(modules);
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+    return this.dataSource;
+  }
+
+  onSelect(module: ContextModule): void {
+    this.selected = module;
+  }
+
 
 }
