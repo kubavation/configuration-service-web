d07e6e5d2bf34c5346a6eebeed89e8728de12153
kubavation <37585627+kubavation@users.noreply.github.com>
2023-05-31 19:32:06 +0200

    temp impl of conext-module configuration

--- configuration-service-web/src/app/context-modules/context-module-configuration/context-module-configuration.component.ts
+++ configuration-service-web/src/app/context-modules/context-module-configuration/context-module-configuration.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {combineLatest, filter, map, switchMap} from "rxjs";
+import {ContextBsService} from "../../shared/context/service/context-bs.service";
+import {ContextModuleConfigurationService} from "./service/context-module-configuration.service";
 
 @Component({
   selector: 'app-context-module-configuration',
@@ -6,5 +10,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./context-module-configuration.component.scss']
 })
 export class ContextModuleConfigurationComponent {
+
+  configuration$ = combineLatest([this.activatedRoute.params, this.contextBsService.context$])
+    .pipe(
+      filter(([params, _]) => !!params['module']),
+      switchMap(([params, context]) => this.contextModuleConfigurationService.moduleConfiguration(context.name, params['module']))
+    )
+
+  constructor(private activatedRoute: ActivatedRoute,
+              private contextModuleConfigurationService: ContextModuleConfigurationService,
+              private contextBsService: ContextBsService) {
+  }
 
 }
